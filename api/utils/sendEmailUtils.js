971130aeab749c981664cd726e49cf9f891a5a62
mvstudent12 <residentIm12@gmail.com>
2024-12-30 12:21:38 -0600

    ammending styling for outlook email services

--- api/utils/sendEmailUtils.js
+++ api/utils/sendEmailUtils.js
@@ -23,20 +23,24 @@ const sendReviewEmail = async (
     subject: `${department} Clearance for Private Industry: ${resident.lastName}, ${resident.firstName} #${resident.residentID}`,
     text: `${department} clearance is needed for resident #${resident.residentID} and is awaiting approval from ${department}. Please review.`,
     html: `
-      <h2>Resident #${resident.residentID} - ${resident.lastName}, ${resident.firstName}</h2>
-      <p>We are awaiting approval from ${department} to place this resident in a job.</p> 
-      <h4>Please approve or deny clearance for this resident.</h4>
+      <h2 style="font-size: 24px; font-weight: bold; color: #333;">Resident #${resident.residentID} - ${resident.lastName}, ${resident.firstName}</h2>
+      <p style="font-size: 16px; color: #333;">We are awaiting approval from ${department} to place this resident in a job.</p> 
+      <h4 style="font-size: 18px; font-weight: bold; color: #333;">Please approve or deny clearance for this resident.</h4>
       <p style="color: blue; font-size: 14px; font-weight: bold;">${notes}</p>
-      <h4>Sent from: ${sender}<h4>
-     <p>
-      <a href="http://kcipi.onrender.com/notification/review/${department}/${resident.residentID}/${sender}"
-         style="display: inline-block; background-color: #4CAF50; color: white; padding: 12px 24px; text-decoration: none; border-radius: 5px; font-size: 16px; text-align: center; font-weight: bold;">
-        Review ${department} Clearance
-      </a>
-     </p>
-     <p>If you are unable to click the button, please copy and paste the following link into your browser:</p>
-    <p><a href="http://kcipi.onrender.com/notification/review/${department}/${resident.residentID}/${sender}">${department} Review Link</a></p>
- 
+      <h4 style="font-size: 16px; font-weight: bold; color: #333;">Sent from: ${sender}</h4>
+      
+      <p>
+        <a href="http://kcipi.onrender.com/notification/review/${department}/${resident.residentID}/${sender}"
+           style="display: inline-block; background-color: #4CAF50; color: white; padding: 12px 24px; text-decoration: none; border-radius: 5px; font-size: 16px; text-align: center; font-weight: bold;">
+          Review ${department} Clearance
+        </a>
+      </p>
+
+      <p style="font-size: 14px; color: #333;">If you are unable to click the button, please copy and paste the following link into your browser:</p>
+      <p style="font-size: 14px; color: #333;">
+        <a href="http://kcipi.onrender.com/notification/review/${department}/${resident.residentID}/${sender}" 
+           style="color: #007BFF; text-decoration: none;">${department} Review Link</a>
+      </p>
     `,
   };
 
