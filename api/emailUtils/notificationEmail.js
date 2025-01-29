34d924919afc7f346fa9889602772f5a917ce2c8
mvstudent12 <residentIm12@gmail.com>
2025-01-29 15:13:14 -0600

    made changes per meeting

--- api/emailUtils/notificationEmail.js
+++ api/emailUtils/notificationEmail.js
@@ -31,7 +31,7 @@ const sendReviewEmail = async (
       
       <p>
       
-        <a href="http://localhost:5999/notification/reviewClearance/${department}/${resident.residentID}/${sender}"
+        <a href="http://kcipi.onrender.com/notification/reviewClearance/${department}/${resident.residentID}/${sender}"
            style="display: inline-block; background-color: #4CAF50; color: white; padding: 12px 24px; text-decoration: none; border-radius: 5px; font-size: 16px; text-align: center; font-weight: bold;">
           Review ${department} Clearance
         </a>
@@ -39,7 +39,7 @@ const sendReviewEmail = async (
 
       <p style="font-size: 14px; color: #333;">If you are unable to click the button, please copy and paste the following link into your browser:</p>
       <p style="font-size: 14px; color: #333;">
-        <a href="http://localhost:5999/notification/reviewClearance/${department}/${resident.residentID}/${sender}" 
+        <a href="http://kcipi.onrender.com/notification/reviewClearance/${department}/${resident.residentID}/${sender}" 
            style="color: #007BFF; text-decoration: none;">${department} Review Link</a>
       </p>
     `,
@@ -118,7 +118,7 @@ const sendRequestInterviewEmail = async (
       
       <p>
       
-        <a href="http://localhost:5999/notification/reviewInterviewRequest/${interviewID}"
+        <a href="http://kcipi.onrender.com/notification/reviewInterviewRequest/${interviewID}"
            style="display: inline-block; background-color: #4CAF50; color: white; padding: 12px 24px; text-decoration: none; border-radius: 5px; font-size: 16px; text-align: center; font-weight: bold;">
           Schedule Interview
         </a>
@@ -126,7 +126,7 @@ const sendRequestInterviewEmail = async (
 
       <p style="font-size: 14px; color: #333;">If you are unable to click the button, please copy and paste the following link into your browser:</p>
       <p style="font-size: 14px; color: #333;">
-        <a href="http://localhost:5999/notification/reviewInterviewRequest/${interviewID}" 
+        <a href="http://kcipi.onrender.com/notification/reviewInterviewRequest/${interviewID}" 
            style="color: #007BFF; text-decoration: none;"><span style="text-transform: capitalize;">${companyName}</span> Review Link</a>
       </p>
     `,
