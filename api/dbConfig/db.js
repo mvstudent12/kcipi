d792a27ed09d7810736c469dbc0910dd69e1193b
mvstudent12 <resident0000m@gmail.com>
2025-02-27 10:41:00 -0600

    fixed bugs from user testing

--- api/dbConfig/db.js
+++ api/dbConfig/db.js
@@ -11,9 +11,9 @@ const dbURI =
 mongoose
   .connect(dbURI, {
     //comment these three out in localhost development vvv
-    // ssl: true, // Ensure SSL is enabled -comment out in development/localhost all three lines
-    // tls: true, // Force TLS connection
-    // tlsInsecure: false, // Optionally, enforce secure connection (recommended)
+    ssl: true, // Ensure SSL is enabled -comment out in development/localhost all three lines
+    tls: true, // Force TLS connection
+    tlsInsecure: false, // Optionally, enforce secure connection (recommended)
   })
   .then(() => console.log("Connected to MongoDB Atlas"))
   .catch((err) => console.error("Error connecting to MongoDB Atlas:", err));
